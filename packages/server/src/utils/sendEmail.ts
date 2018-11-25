e495fb4f0501c7b93e6ecfed5b162fb6163d202b
SabithHaneef <sabith.th.07@gmail.com>
2018-11-25 17:46:26 +0530

    removed sparkpost, replaced with nodemailer to send emails

--- packages/server/src/utils/sendEmail.ts
+++ packages/server/src/utils/sendEmail.ts
@@ -1,23 +1,33 @@
-import * as SparkPost from "sparkpost";
-
-const client = new SparkPost(process.env.SPARKPOST_API_KEY || "API_KEY");
+import * as nodemailer from "nodemailer";
 
 export const sendEmail = async (receipient: string, url: string) => {
-  const response = await client.transmissions.send({
-    options: {
-      sandbox: true
-    },
-    content: {
-      from: "testing@sparkpostbox.com",
-      subject: "GraphQL Registration Confirmation Email",
-      html: `<html>
-          <body>
-          <p>Testing SparkPost - the world's most awesomest email service!</p>
-          <a href="${url}">Confirm Email</a>
-          </body>
-          </html>`
-    },
-    recipients: [{ address: receipient }]
+  const transporter = nodemailer.createTransport({
+    host: "smtp.ethereal.email",
+    port: 587,
+    auth: {
+      user: process.env.NODEMAILER_USERNAME,
+      pass: process.env.NODEMAILER_PASSWORD
+    }
   });
-  console.log(response);
+
+  const mailOptions = {
+    from: "admin@airbnb-clone.com",
+    to: receipient,
+    subject: "Airbnb Clone Confirmation Email",
+    html: `<html>
+              <body>
+                <p>Please follow the below link to verify your account.</p>
+                <a href="${url}">Confirm Email</a>
+                <p>Thank you for signing up with Airbnb clone.</p>
+              </body>
+            </html>`
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      return console.log(error);
+    }
+    console.log(`Message sent: ${info.messageId}`);
+    console.log(`Preview URL: ${nodemailer.getTestMessageUrl(info)}`);
+  });
 };
